10ffde0e1c5be44fb9e8c899d16cabbbb69705f3
Krumlov <62485575+Stakepy@users.noreply.github.com>
2025-07-01 18:45:09 +0200

    Update Kasseta.py

--- Kasseta.py
+++ Kasseta.py
@@ -42,9 +42,10 @@ ytdl_format_options = {
 }
 
 ffmpeg_options = {
-    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5 -analyzeduration 10M -probesize 10M',
     'options': '-vn'
 }
+
 
 YTDL = youtube_dl.YoutubeDL(ytdl_format_options)
 
